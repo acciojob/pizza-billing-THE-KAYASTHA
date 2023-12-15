c03cdc1a27bcfccff2c669b2e0151bedae6d7b27
kamal <tekkensingle@gmail.com>
2023-12-15 17:45:15 +0530

    changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,10 +4,26 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill="";
+
+    public Boolean isCheese = false;
+
+    public Boolean isTopping = false;
+
+    public Boolean isBag = false;
+
+    public Boolean isBill = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+            this.price=300;
+            this.bill=this.bill+"Base Price Of The Pizza: 300"+"\n";
+        }
+        else{
+            this.price=400;
+            this.bill=this.bill+"Base Price Of The Pizza: 400"+"\n";
+        }
         // your code goes here
     }
 
@@ -16,19 +32,48 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+       if(!this.isCheese){
+           this.price=this.price+80;
+           bill=bill+"Extra Cheese Added: 80"+"\n";
+           this.isCheese=true;
+
+       }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!this.isTopping) {
+            if (isVeg) {
+                this.price = this.price + 70;
+                bill = bill + "Extra Toppings Added: 70" + "\n";
+            } else {
+                this.price = this.price + 120;
+                bill = bill + "Extra Toppings Added: 120" + "\n";
+            }
+            isTopping = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!this.isBag) {
+            this.price = this.price + 20;
+            bill = bill + "Paperbag Added: 20" + "\n";
+            isBag = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!isBill) {
+            bill = bill + "Total Price: " + this.price + "\n";
+            this.isBill = true;
+            return this.bill;
+
+        }
+        else {
+            return this.bill;
+        }
+
     }
 }
